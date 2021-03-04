005b49429b8d03ebb8547fc2e96f96fb4071ad0a
sumin2123 <mini247269@naver.com>
2021-03-04 22:37:35 +0900

    Chap03마지막

--- Chap03App/Chap03App/VarTestApp/Program.cs
+++ Chap03App/Chap03App/VarTestApp/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace VarTestApp
 {
     class Program
     {
+        static float i = 100.5f;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"값은 {i}입니다.");
         }
     }
 }
